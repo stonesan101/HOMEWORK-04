7a13c2a38fd079c90ac10d8110f7b4ff56525fc2
stonesan101 <estonesan88.es@gmail.com>
2026-02-26 13:08:13 -0600

    Light overrides

--- src/Light.java
+++ src/Light.java
@@ -1,6 +1,6 @@
 public class Light extends SmartDevice{
-    private String deviceName;
-    private String type;
+    final String deviceName;
+    final String type;
     private int brightnessLevel;
     private String status;
 
@@ -9,6 +9,25 @@ public class Light extends SmartDevice{
         this.type = "Light";
         this.brightnessLevel = 0;
         this.status = "Off";
+    }
+
+    public void turnOn(int brightnessLevel) {
+        this.brightnessLevel = brightnessLevel;
+        status = "On";
+    }
+
+    public void turnOff() {
+        status = "Off";
+        this.brightnessLevel = 0;
+    }
+
+    public void displayDeviceStatus() {
+        System.out.println("Device: " + deviceName);
+        System.out.println("Type: " + type);
+        System.out.println("Status: " + status);
+        if (status.equals("On")) {
+            System.out.println("Brightness Level: " + brightnessLevel);
+        }
     }
 
 }
